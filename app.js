671aad498c9425f97d66203d31d8d5c7318406d5
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-06 14:31:29 +0530

    wrote the full code

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var url = "https://api.funtranslations.com/translate/minion.json"
-console.log(url);
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL = "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("Error occured", error);
+    alert("Something went wrong with server! Try again after some time")
+}
 
 function clickHandler() {
-    outputDiv.innerText = " " + txtInput.value; // didnt understand innerText
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedTxt = json.contents.translated;
+        outputDiv.innerText = translatedTxt;
+    })
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
