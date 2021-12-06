824eb79d9b8816497172cd5446fa2476eb0ca091
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-06 10:16:54 +0530

    tested brackets

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked!")
-    console.log("input", txtInput.value)
+    console.log("clicked!");
+    console.log("input", txtInput.value);
 })
