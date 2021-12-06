fbf24daddfdf17594ca87d4975e699fa402976d1
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-06 14:06:37 +0530

    added minions API

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var url = "https://api.funtranslations.com/translate/minion.json"
+console.log(url);
+
 function clickHandler() {
-    outputDiv.innerText = "sdfhksjdfhsf " + txtInput.value; // didnt understand innerText
+    outputDiv.innerText = " " + txtInput.value; // didnt understand innerText
 };
 
 btnTranslate.addEventListener("click", clickHandler)
